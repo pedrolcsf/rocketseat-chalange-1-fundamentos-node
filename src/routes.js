715dd3a43b1f6be66e3915900b896839d72e9592
Pedro Ferreira <pedrolcsferreira@gmail.com>
2023-02-11 22:37:16 -0400

    basic routes added

--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,58 @@ import { buildRoutePath } from './utils/build-route-path.js'
 const database = new Database()
 
 export const routes = [
+  {
+    method: 'POST',
+    path: buildRoutePath('/tasks'),
+    handler: (req, res) => {
+      const { title, description } = req.body
+
+      const task = {
+        id: randomUUID(),
+        title,
+        description,
+        completed_at: null,
+        created_at: new Date(),
+        updated_at: new Date()
+      }
   
+      database.insert('tasks', task)
+  
+      return res.writeHead(201).end()
+    }
+  },
+  {
+    method: 'GET',
+    path: buildRoutePath('/tasks'),
+    handler: (req, res) => {
+      const tasks = database.select('tasks')
+
+      return res.end(JSON.stringify(tasks))
+    }
+  },
+  {
+    method: 'PUT',
+    path: buildRoutePath('/tasks/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+      const { title, description } = req.body
+      database.update('tasks', id, {
+        title,
+        description,
+        updated_at: new Date()
+      })
+      
+      return res.writeHead(204).end()
+    }
+  },
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/tasks/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+      database.delete('tasks', id)
+
+      return res.writeHead(204).end()
+    }
+  },
 ]
